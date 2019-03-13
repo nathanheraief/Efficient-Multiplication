18ff04f4929cae6044d67b586e67c7ee6d94a12c
Nathan Heraief <nathan.heraief@gmail.com>
2019-03-13 16:06:02 +0100

    algo fixed

--- src/hw/Omura Addition/Omura_Optimized.vhd
+++ src/hw/Omura Addition/Omura_Optimized.vhd
@@ -33,13 +33,12 @@ ENTITY Omura_Optimized IS
 
 		--Custom I/O
 		sub_i : IN std_logic;
-		p_i   : IN std_logic_vector(N - 1 DOWNTO 0);
-		m_i   : IN std_logic_vector(N + 1 DOWNTO 0)
+		p_i   : IN std_logic_vector(N - 1 DOWNTO 0)
 	);
 END Omura_Optimized;
 
 ARCHITECTURE rtl OF Omura_Optimized IS
-	TYPE STATE_T IS (INIT, PREPROCESS, CALCUL, RESCALE, WRITE); --Vous pouvez rajouter des etats ici.
+	TYPE STATE_T IS (INIT, PREPROCESS, CALCUL, MODULO, RESCALE, WRITE); --Vous pouvez rajouter des etats ici.
 
 	SIGNAL current_s     : STATE_T;
 	SIGNAL dataa_p       : STD_LOGIC_VECTOR(N + 1 DOWNTO 0);
@@ -49,10 +48,14 @@ ARCHITECTURE rtl OF Omura_Optimized IS
 	SIGNAL to_be_written : STD_LOGIC_VECTOR(N + 1 DOWNTO 0);
 	SIGNAL S             : STD_LOGIC_VECTOR(N + 1 DOWNTO 0);
 	SIGNAL Sp            : STD_LOGIC_VECTOR(N + 1 DOWNTO 0);
-	SIGNAL pp            : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+	SIGNAL p_s            : STD_LOGIC_VECTOR(N +1  DOWNTO 0);
+	SIGNAL np_s          : STD_LOGIC_VECTOR(N +1  DOWNTO 0);
+	SIGNAL m             : STD_LOGIC_VECTOR(N + 1 DOWNTO 0);
+--SIGNAL debug         :  STD_LOGIC_VECTOR(N +1  DOWNTO 0);
 	SIGNAL busy          : STD_LOGIC;
 	SIGNAL T1            : STD_LOGIC;
 	SIGNAL T2            : STD_LOGIC;
+	
 
 BEGIN
 
@@ -66,13 +69,16 @@ BEGIN
 			to_be_written <= (OTHERS => '0');
 			S             <= (OTHERS => '0');
 			Sp            <= (OTHERS => '0');
-			pp            <= (OTHERS => '0');
+			p_s           <= (OTHERS => '0');
+			np_s           <= (OTHERS => '0');
+			m             <= (OTHERS => '0');			
 			busy          <= '0';
 			result        <= (OTHERS => '0');
 			done          <= '0';
 			T1            <= '0';
 			T2            <= '0';
 			current_s     <= Init;
+			
 
 		ELSIF (rising_edge(clk)) THEN
 
@@ -81,8 +87,11 @@ BEGIN
 				WHEN INIT =>
 					IF (start = '1' AND busy = '0') THEN
 						done      <= '0';
+						m(n)      <='1';
 						dataa_p   <= (N + 1 DOWNTO dataa'length => '0') & dataa;
 						datab_p   <= (N + 1 DOWNTO datab'length => '0') & datab;
+						p_s       <= "00" & p_i;
+						np_s      <= not ("00"&p_i) +1;
 						current_s <= PREPROCESS;
 					ELSE
 						done      <= '0';
@@ -90,6 +99,7 @@ BEGIN
 					END IF;
 
 				WHEN PREPROCESS =>
+				    m             <= m  +np_s;
 					IF (sub_i = '1') THEN
 						datab_f   <= NOT datab_p + 1;
 						dataa_f   <= dataa_p;
@@ -102,16 +112,19 @@ BEGIN
 
 				WHEN CALCUL =>
 					S                  <= dataa_f + datab_f;
-					pp(N - 1 DOWNTO 1) <= p_i(N - 2 DOWNTO 0);
-					current_s          <= RESCALE;
+					current_s          <= MODULO;
+					
+				WHEN MODULO =>
+				    Sp <= S + m;
+				    current_s          <= RESCALE;
 
 				WHEN RESCALE =>
 					IF (S(N + 1) = '1') THEN
 						to_be_written <= NOT S;
 						current_s     <= WRITE;
 					ELSE
-						IF (S(N) = '1') THEN
-							to_be_written <= S + (NOT pp + 1);
+						IF (Sp(N) = '1') THEN
+							to_be_written <= Sp - 2**n;
 							current_s     <= WRITE;
 						ELSE
 							to_be_written <= S;
